81ddc8f3422747f8a61b2933d99d101eb21fa54c
Krzysztof Krzepicki <krzysztof.krzepicki@gmail.com>
2017-12-25 18:54:50 +0100

    adding menu and minified images

---
+++ js/stickyNavBar.js
@@ -0,0 +1,21 @@
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("nav__main");
+var navMainPadding = document.getElementById("nav__main--padding");
+//var navbar = document.getElementsByClassName("nav__main");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+    navMainPadding.classList.add("nav__margin")
+  } else {
+    navbar.classList.remove("sticky");
+    navMainPadding.classList.remove("nav__margin");
+  }
+}
