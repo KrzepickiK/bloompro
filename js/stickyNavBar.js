60ff59d4fc065bdddfcb52b1de5cb99b4bd98c7f
Krzysztof Krzepicki <krzysztof.krzepicki@gmail.com>
2018-01-02 17:37:35 +0100

    commit testowy platnosci

--- js/stickyNavBar.js
+++ js/stickyNavBar.js
@@ -4,6 +4,7 @@ window.onscroll = function() {myFunction()};
 // Get the navbar
 var navbar = document.getElementById("nav__main");
 var navMainPadding = document.getElementById("nav__main--padding");
+var navMainLogo = document.getElementById("nav__main--logo");
 //var navbar = document.getElementsByClassName("nav__main");
 
 // Get the offset position of the navbar
@@ -14,8 +15,10 @@ function myFunction() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
     navMainPadding.classList.add("nav__margin")
+    navMainLogo.classList.add("visible")
   } else {
     navbar.classList.remove("sticky");
     navMainPadding.classList.remove("nav__margin");
+    navMainLogo.classList.remove("visible")
   }
 }
